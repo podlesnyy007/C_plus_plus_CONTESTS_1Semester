4ae8e7894d35861f9bb37279a7a584e1ae17b823
podlesnyy007 <114169264+podlesnyy007@users.noreply.github.com>
2022-11-25 01:20:53 +0400

    Create 8) ЕГЭ. Задача 1.cpp

---
+++ 4.Циклы 1/8) ЕГЭ. Задача 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, max1 = -10000, max2 = -10000;
+    cin >> n;
+    while (n != 0) {
+        int x;
+        cin >> x;
+        if (x > max1) {
+            max2 = max1;
+            max1 = x;
+        }
+        else if (x > max2 && x != max1)
+            max2 = x;
+        n--;
+    }
+    cout << max2;
+}
