24320001de972f54bc2ad8d7e684b9d0d0b55628
podlesnyy007 <114169264+podlesnyy007@users.noreply.github.com>
2022-11-24 22:41:33 +0400

    Create 3) Циклы с параметром. Задача 3.cpp

---
+++ 4.Циклы 1/3) Циклы с параметром. Задача 3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m = -1, t = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        int x;
+        cin >> x;
+        if (abs(x) >= m) {
+            m = abs(x);
+            t = i;
+        }
+    }
+    cout << t;
+}
