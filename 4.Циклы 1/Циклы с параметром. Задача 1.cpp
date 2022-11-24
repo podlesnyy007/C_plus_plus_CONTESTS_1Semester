fff9a50a6793e311eee375208e6d8ca1036869a0
podlesnyy007 <114169264+podlesnyy007@users.noreply.github.com>
2022-11-24 21:33:29 +0400

    Create Циклы с параметром. Задача 1.cpp

---
+++ 4.Циклы 1/Циклы с параметром. Задача 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, k = -1000000;
+	cin >> n;
+	while (n>0) {
+		int x;
+		cin >> x;
+		if (x > k)
+			k = x;
+		n--;
+	}
+	cout << k;
+}
