1f08191686bbb6cfe979d719e44a6d86e1ecab3d
podlesnyy007 <114169264+podlesnyy007@users.noreply.github.com>
2022-11-25 00:11:28 +0400

    Create 7) Практические задачи. Задача 3.cpp

---
+++ 4.Циклы 1/7) Практические задачи. Задача 3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+int main()
+{
+    int n, c;
+    cin >> n >> c;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            if (i % c == 0)
+                cout << i << " ";
+        }
+    }
+}
