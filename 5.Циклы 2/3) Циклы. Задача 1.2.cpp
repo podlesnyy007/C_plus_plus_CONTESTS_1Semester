b100f2d21c6d9ec92d8ebbb8bb671c64ca1c9ccf
podlesnyy007 <114169264+podlesnyy007@users.noreply.github.com>
2022-12-10 11:53:42 +0400

    Update 3) Циклы. Задача 1.2.cpp

--- 5.Циклы 2/3) Циклы. Задача 1.2.cpp
+++ 5.Циклы 2/3) Циклы. Задача 1.2.cpp
@@ -1,10 +1,14 @@
+/*
+Дано натуральное число N. Необходимо разложить его на простые множители.
+*/
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int n, i = 2, f = 0;
-    cin >> n;
+    int n, i = 2; // i=2, т.к 1 не является простым числом
+    cin >> n; // натуральное число N
     while (i <= n){
         while (n % i == 0){
             n /= i;
